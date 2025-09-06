1c88cca60839ecf8f43422026b6a49308b3895d0
Maxim <maximconod@gmail.com>
2025-09-06 17:52:21 +0200

    Extract video into frames

---
+++ src/detect_ball.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+cap = cv.VideoCapture("C:/Users/maxim/Documents/Uni_Projects/SpeedTracker/Data/BallExchange.mp4")
+
+# Needing to extract one frame after another
+while True:
+    ret, frame = cap.read()
+
+    cv.imshow("Frame", frame)
+
+    key = cv.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
