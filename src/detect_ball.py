f9a051e9cbe5d268274fc678e8e65d6158d2da05
Maxim <maximconod@gmail.com>
2025-09-09 00:28:40 +0200

    Test different detection models

--- src/detect_ball.py
+++ src/detect_ball.py
@@ -1,6 +1,16 @@
 from ultralytics import YOLO
-import cv2
+import cv2 as cv
 
-model = YOLO("yolov8n.pt")
-results = model("C:/Users/maxim/Documents/Uni_Projects/SpeedTracker/Data/pic2_tennis.jpg", show=True)
-cv2.waitKey(0)
+model = YOLO("../Yolo-Weights/yolo11n.pt")
+results = model("../Data/pic3_tennis.png", show=True)
+
+model2 = YOLO("../Yolo-Weights/yolov8n.pt")
+results2 = model2("../Data/pic3_tennis.png", show=True)
+
+# Results
+new_img = results[0].plot()
+cv.imshow("New version", new_img)
+
+new_img2 = results2[0].plot()
+cv.imshow("Old version", new_img2)
+cv.waitKey(0)
